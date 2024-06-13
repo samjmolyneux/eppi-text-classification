91dafd8a4ef2ab89d65c50180fdf98085afb671b
Sam Molyneux <samjmolyneux@gmail.com>
2024-06-13 21:05:04 +0100

    functioning parallel spacy processing

--- eppi_text_classification/save_features_labels.py
+++ eppi_text_classification/save_features_labels.py
@@ -1,14 +1,15 @@
 from joblib import Parallel, delayed
 import pandas as pd
 import spacy
-import time
 from sklearn.feature_extraction.text import TfidfVectorizer
 import os
 
+# Considerations: Setting the joblib backend,
+#                Choosing spacy model,
 
+
+# TO DO:Loguru for processer count and chunksize
 system_num_processes = os.cpu_count()
-
-# nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
 
 
 def lemmatize_pipe(doc):
@@ -22,7 +23,7 @@ def lemmatize_pipe(doc):
 
 def chunker(iterable, total_length, process_count):
     chunksize = -(-total_length // process_count)  # ceiling division
-    # it doesnt repeat the first elements if the final chunk doesnt fit
+    print(chunksize)
     return (
         iterable[pos : pos + chunksize] for pos in range(0, total_length, chunksize)
     )
@@ -34,60 +35,65 @@ def flatten(list_of_lists):
 
 def process_chunk(texts):
     nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-    preproc_pipe = []
-
-    # Batch_size needs managing for deployment
-    [lemmatize_pipe(doc) for doc in nlp.pipe(texts, batch_size=100)]
-    return preproc_pipe
+    return [lemmatize_pipe(doc) for doc in nlp.pipe(texts, batch_size=25)]
 
 
-def preprocess_parallel(texts, process_count=system_num_processes):
-    executor = Parallel(
-        n_jobs=process_count, backend="multiprocessing", prefer="processes"
-    )
-    do = delayed(process_chunk)
+def process_column(texts, process_count=system_num_processes):
+    print(process_count)
     tasks = (
-        do(chunk) for chunk in chunker(texts, len(texts), process_count=process_count)
+        delayed(process_chunk)(chunk)
+        for chunk in chunker(texts, len(texts), process_count=process_count)
     )
-    result = executor(tasks)
+    result = Parallel(n_jobs=process_count, backend="loky")(tasks)
     return flatten(result)
 
 
-def get_features(df):
-    abstracts = preprocess_parallel(df["abstract"])
-    titles = preprocess_parallel(df["title"])
+def get_features(abstract_column, title_column):
+    abstracts = process_column(abstract_column)
+    titles = process_column(title_column)
     all_words = []
     for abstract, title in zip(abstracts, titles, strict=True):
         words = [f"t_{word}" for word in title] + [f"a_{word}" for word in abstract]
         all_words.append(words)
 
-    vectorizer = TfidfVectorizer(
-        ngram_range=(1, 3),
-        max_features=75000,
-        min_df=3,
-        strip_accents="unicode",
-        token_pattern=r"(?u)\S+",
+    return all_words
+
+
+def get_labels(label_column):
+    labels = label_column.tolist()
+    labels = [int(label) for label in labels]
+    return labels
+
+
+def get_features_and_labels(
+    df, title_col_name="title", abstract_col_name="abstract", y_col_name="included"
+):
+    df = df.rename(
+        columns={
+            title_col_name: "title",
+            abstract_col_name: "abstract",
+        }
     )
-    features = vectorizer.fit_transform(all_words)
-    return features
-
-
-def get_features_and_labels(df, title_col_name="title", abstract_col_name="abstract"):
-    df = df.rename(columns={title_col_name: "title", abstract_col_name: "abstract"})
     df = df.dropna(subset=["title", "abstract"], how="all")
-
     df["abstract"] = df["abstract"].astype(str)
     df["title"] = df["title"].astype(str)
 
-    tfidf_scores = get_features(df)
+    word_features = get_features(df["abstract"], df["title"])
 
-    # Should make code here for loading labels of any type using a dictionary of some sort
-    labels = df["included"].tolist()
-    labels = [int(label) for label in labels]
+    labels = get_labels(df[y_col_name])
 
-    return tfidf_scores, labels
+    return word_features, labels
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/raw/debunking_review.tsv", sep="\t")
+    features, labels = get_features_and_labels(df)
+
+    # df = pd.read_csv("../../data/raw/studytype_multiclass.tsv", sep="\t")
+    # tfidf_scores, labels = get_features_and_labels(
+    #     df, abstract_col_name="AB", title_col_name="TI"
+    # )
 
 
 # NEED TO MAKE SURE THAT THIS CODE WONT DELETE THE EMPTY ELEMENTS FOR SOME REASON
-# ALSO NEED TO UNDERSTAND WHATS GOING ON WITH THE FLATTEN
 # ALSO NEED TO MAKE SURE THAT THEY COME BACK OUR IN THE CORRECT ORDER (THE MULTIPROCESSING MIGHT MESS UP THE ORDER OF THE ELEMENTS)
