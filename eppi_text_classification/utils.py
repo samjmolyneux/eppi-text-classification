017cc95fc5aacd0cbe5e70ee744d0f8bec516808
Sam Molyneux <samjmolyneux@gmail.com>
2024-06-14 17:21:54 +0100

    added delete study function

--- eppi_text_classification/utils.py
+++ eppi_text_classification/utils.py
@@ -1,4 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+from pathlib import Path
+import optuna
 
 
 def get_tfidf_and_names(word_features, min_df=3, max_features=75000):
@@ -14,3 +16,10 @@ def get_tfidf_and_names(word_features, min_df=3, max_features=75000):
     feature_names = vectorizer.get_feature_names_out()
 
     return tfidf_scores, feature_names
+
+
+def delete_optuna_study(study_name):
+    root_path = Path(Path(__file__).resolve()).parent.parent
+    db_storage_url = f"sqlite:///{root_path}/optuna.db"
+
+    optuna.delete_study(study_name=study_name, storage=db_storage_url)
