fc1f674d544e0ab3cbc4dae2096b7f2a5235de6e
Sam Molyneux <samjmolyneux@gmail.com>
2024-06-22 13:16:41 +0100

    refactor

--- eppi_text_classification/shap_plotter.py
+++ eppi_text_classification/shap_plotter.py
@@ -42,71 +42,69 @@ class ShapPlotter:
 
         self.background_data = np.zeros(shape=(1, X_test.shape[-1]))
 
-        if isinstance(model, LGBMClassifier | XGBClassifier):
-            if not tree_path_dependent:
-                self.explainer = shap.TreeExplainer(
-                    model,
-                    data=self.background_data,
-                    model_output="probability",
-                )
-            else:
-                self.explainer = shap.TreeExplainer(
-                    model,
-                    model_output="raw",
-                    feature_perturbation="tree_path_dependent",
-                )
+        if not tree_path_dependent:
+            self.explainer = shap.TreeExplainer(
+                model,
+                data=self.background_data,
+                model_output="probability",
+            )
+        else:
+            self.explainer = shap.TreeExplainer(
+                model,
+                model_output="raw",
+                feature_perturbation="tree_path_dependent",
+            )
 
         if subsample is not None:
             self.X_test = shap.sample(self.X_test, subsample)
 
         self.shap_values = self.explainer.shap_values(self.X_test)
 
+        if isinstance(model, RandomForestClassifier):
+            self.shap_values = self.shap_values[:, :, 1]
+
     def dot_plot(self, num_display=10, log_scale=True, plot_zero=False):
-        if isinstance(self.model, LGBMClassifier | XGBClassifier):
-            summary_new(
-                self.shap_values,
-                self.X_test,
-                feature_names=self.feature_names,
-                max_display=num_display,
-                color_bar_label="tfidf value",
-                plot_type="dot",
-                use_log_scale=log_scale,
-                plot_size=(20, 0.4 * num_display),
-                plot_zero=plot_zero,
-            )
+        summary_new(
+            self.shap_values,
+            self.X_test,
+            feature_names=self.feature_names,
+            max_display=num_display,
+            color_bar_label="tfidf value",
+            plot_type="dot",
+            use_log_scale=log_scale,
+            plot_size=(20, 0.4 * num_display),
+            plot_zero=plot_zero,
+        )
 
     def bar_chart(self, num_display=10):
-        if isinstance(self.model, LGBMClassifier | XGBClassifier):
-            summary_new(
-                self.shap_values,
-                self.X_test,
-                feature_names=self.feature_names,
-                plot_type="bar",
-                max_display=num_display,
-            )
+        summary_new(
+            self.shap_values,
+            self.X_test,
+            feature_names=self.feature_names,
+            plot_type="bar",
+            max_display=num_display,
+        )
 
     # TO DO: Double check that the use of threshold is right for the base value
     def decision_plot(self, threshold, num_display=10):
-        if isinstance(self.model, LGBMClassifier | XGBClassifier):
-            decision(
-                threshold,
-                self.shap_values,
-                self.X_test,
-                feature_names=self.feature_names,
-                feature_display_range=slice(-1, -num_display, -1),
-                ignore_warnings=True,
-            )
+        decision(
+            threshold,
+            self.shap_values,
+            self.X_test,
+            feature_names=self.feature_names,
+            feature_display_range=slice(-1, -num_display, -1),
+            ignore_warnings=True,
+        )
 
     def single_decision_plot(self, threshold, index, num_display=10):
-        if isinstance(self.model, LGBMClassifier | XGBClassifier):
-            decision(
-                threshold,
-                self.shap_values[index],
-                self.X_test,
-                feature_names=self.feature_names,
-                feature_display_range=slice(-1, -num_display, -1),
-                ignore_warnings=True,
-            )
+        decision(
+            threshold,
+            self.shap_values[index],
+            self.X_test,
+            feature_names=self.feature_names,
+            feature_display_range=slice(-1, -num_display, -1),
+            ignore_warnings=True,
+        )
 
 
 def shorten_text(text, length_limit):
