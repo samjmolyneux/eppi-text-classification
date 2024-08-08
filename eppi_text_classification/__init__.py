367bbb25347a2540beba9d38f79af455264712c9
Sam Molyneux <samjmolyneux@gmail.com>
2024-08-08 12:15:32 +0100

    make all classes and function accessible from base

--- eppi_text_classification/__init__.py
+++ eppi_text_classification/__init__.py
@@ -3,7 +3,10 @@
 from .opt import OptunaHyperparameterOptimisation, delete_optuna_study
 from .plotly_confusion import binary_train_valid_confusion_plotly
 from .plotly_roc import plotly_roc
+from .plots import binary_train_valid_confusion_plot
+from .predict import get_raw_threshold, predict_scores, raw_threshold_predict
 from .save_features_labels import get_features_and_labels
+from .shap_plotter import ShapPlotter
 from .utils import get_tfidf_and_names
 
 __all__ = [
@@ -13,4 +16,10 @@ __all__ = [
     "binary_train_valid_confusion_plotly",
     "plotly_roc",
     "delete_optuna_study",
+    "predict_scores",
+    "plotly_roc",
+    "get_raw_threshold",
+    "raw_threshold_predict",
+    "binary_train_valid_confusion_plot",
+    "ShapPlotter",
 ]
