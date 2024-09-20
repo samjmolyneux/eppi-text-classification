7e56a5d999974682f90bec3b378c689f6a9a6c62
Sam Molyneux <samjmolyneux@gmail.com>
2024-09-20 15:11:40 +0100

    updates to pipelines

--- pipelines/components/train_model/train_model.py
+++ pipelines/components/train_model/train_model.py
@@ -1,16 +1,20 @@
 
 import argparse
+import json
 import os
 
-import json
+import joblib
 import jsonpickle
-import numpy as np
 from lightgbm import LGBMClassifier
-from scipy.sparse import load_npz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from xgboost import XGBClassifier
-import joblib
+
+from eppi_text_classification.utils import (
+    load_csr_at_directory,
+    load_json_at_directory,
+    load_np_array_at_directory,
+)
 
 mname_to_mclass = {
     "SVC": SVC,
@@ -45,19 +49,21 @@ def main():
     )
     args = parser.parse_args()
 
-    X_train = load_npz(os.path.join(args.X_train, "X_train.npz"))
-    y_train = np.load(os.path.join(args.y_train, "y_train.npy"))
-    model_params_path = os.path.join(args.model_parameters, "model_params.json")
-    with open(model_params_path, "r") as file:
-        json_model_parameters = json.load(file)
-    model_parameters = jsonpickle.decode(json_model_parameters)
+    X_train = load_np_array_at_directory(args.X_train)
+    y_train = load_csr_at_directory(args.y_train)
+    model_parameters = load_json_at_directory(args.model_parameters)
+    # model_params_path = os.path.join(args.model_parameters, "model_params.json")
+    # with open(model_params_path, "r") as file:
+    #     json_model_parameters = json.load(file)
+    # model_parameters = jsonpickle.decode(json_model_parameters)
 
     model_class = mname_to_mclass[model_parameters.pop("model_name")]
     model = model_class(**model_parameters)
 
     model.fit(X_train, y_train)
 
-    joblib.dump(model, os.path.join(args.model, "model.joblib")) 
+    joblib.dump(model, os.path.join(args.model, "model.joblib"))
+
 
 if __name__ == "__main__":
     main()
