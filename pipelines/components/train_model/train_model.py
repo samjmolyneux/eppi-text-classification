30ea39dfbd673fc5f592aaff4b133c7f226fe74d
Sam Molyneux <samjmolyneux@gmail.com>
2024-09-20 16:42:58 +0100

    update components

--- pipelines/components/train_model/train_model.py
+++ pipelines/components/train_model/train_model.py
@@ -49,8 +49,8 @@ def main():
     )
     args = parser.parse_args()
 
-    X_train = load_np_array_at_directory(args.X_train)
-    y_train = load_csr_at_directory(args.y_train)
+    X_train = load_csr_at_directory(args.X_train)
+    y_train = load_np_array_at_directory(args.y_train)
     model_parameters = load_json_at_directory(args.model_parameters)
     # model_params_path = os.path.join(args.model_parameters, "model_params.json")
     # with open(model_params_path, "r") as file:
