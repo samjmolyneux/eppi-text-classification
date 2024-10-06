d88179d46365a4116c37e81fa231730d5aebae7c
Sam Molyneux <samjmolyneux@gmail.com>
2024-10-06 15:17:27 +0100

    add primtive functionality to component scripts

---
+++ pipelines/primitive_components/create_dot_plot/create_dot_plot.py
@@ -0,0 +1,81 @@
+
+import argparse
+import json
+import os
+from pathlib import Path
+
+from eppi_text_classification.shap_plotter import DotPlot
+from eppi_text_classification.utils import (
+    load_csr_at_directory,
+    load_np_array_at_directory,
+)
+
+
+def main():
+    # input and output arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--shap_values",
+        type=str,
+        help="path to shap_plotter",
+    )
+    parser.add_argument(
+        "--X",
+        type=str,
+        help="path to X data to explain model on",
+    )
+    parser.add_argument(
+        "--feature_names",
+        type=str,
+        help="path to features names",
+    )
+    parser.add_argument(
+        "--num_display",
+        type=int,
+        help="number of features to display on plot",
+        default=10,
+    )
+    parser.add_argument(
+        "--log_scale",
+        type=bool,
+        help="bool of whether to display plot along log scale",
+        default=False,
+    )
+    parser.add_argument(
+        "--plot_zero",
+        type=bool,
+        help="bool of whether to plot zero shap values",
+        default=False,
+    )
+    parser.add_argument(
+        "--dot_plot",
+        type=str,
+        help="path to dot plot",
+    )
+    args = parser.parse_args()
+
+    shap_values = load_csr_at_directory(args.shap_values)
+    X = load_csr_at_directory(args.X)
+    feature_names = load_np_array_at_directory(args.feature_names, allow_pickle=True)
+    num_display = args.num_display
+    log_scale = args.log_scale
+    plot_zero = args.plot_zero
+
+    dot_plot = DotPlot(
+        shap_values=shap_values,
+        X_test=X,
+        feature_names=feature_names,
+        num_display=num_display,
+        log_scale=log_scale,
+        plot_zero=plot_zero,
+    )
+
+    print(f"feature_names : {feature_names.shape}")
+    print(f"shap_values : {shap_values.shape}")
+    print(f"X : {X.shape}")
+    dot_plot_path = Path(args.dot_plot) / "dot_plot.png"
+    dot_plot.save(dot_plot_path)
+
+
+if __name__ == "__main__":
+    main()
