f85e041aaef9f2de9c32b72dfd3e32f102740099
Sam Molyneux <samjmolyneux@gmail.com>
2025-02-16 19:10:17 +0000

    fix sinlge processor bug

--- eppi_text_classification/train.py
+++ eppi_text_classification/train.py
@@ -19,9 +19,12 @@ def train(
     params: dict[str, Any],
     X: "csr_matrix",
     y: NDArray[np.int64],
+    n_jobs: int = -1,
 ) -> NDArray[np.float64] | NDArray[np.float32]:
+    train_params = copy.deepcopy(params)
+    train_params["n_jobs"] = n_jobs 
+
     if model_name == "lightgbm":
-        train_params = copy.deepcopy(params)
         with SuppressStderr(
             [
                 "No further splits with positive gain, best gain: -inf",
@@ -34,7 +37,6 @@ def train(
         return model
 
     if model_name == "xgboost":
-        train_params = copy.deepcopy(params)
         num_boost_round = train_params.pop("n_estimators")
         # enable categorical is always false
         enable_categorical = train_params.pop("enable_categorical")
@@ -46,19 +48,21 @@ def train(
             num_boost_round=num_boost_round,
         )
         # gblinear uses a different shap explainer, so we save booster info
-        model.set_attr(boosting_type=params["booster"])
+        model.set_attr(boosting_type=train_params["booster"])
         return model
 
     if model_name == "RandomForestClassifier":
-        model = RandomForestClassifier(**params)
+        model = RandomForestClassifier(**train_params)
         model.fit(X, y)
         return model
 
     if model_name == "SVC":
-        model = SVC(**params)
+        model = SVC(**train_params)
         model.fit(X, y)
         return model
 
     print(f"model is {model}")
     msg = "Model not recognised."
     raise ValueError(msg)
+
+
