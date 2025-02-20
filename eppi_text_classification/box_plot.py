aa3b3d7cfa3ad8d6809f6ba95c748938d501e66a
Sam Molyneux <samjmolyneux@gmail.com>
2025-02-20 20:14:51 +0000

    add more colours to pallete

--- eppi_text_classification/box_plot.py
+++ eppi_text_classification/box_plot.py
@@ -152,14 +152,35 @@ def generate_box_plot_html(
         <script>
             function createBoxPlot(dataList, nameList, title, xaxisTitle, yaxisTitle, divId) {{
                 const fillPalette = [
-                    "rgba(0, 0, 255, 0.5)",
-                    "rgba(255, 0, 0, 0.5)",
-                    "rgba(0, 200, 0, 0.5)",
-                    "rgba(255, 165, 0, 0.5)",
-                    "rgba(128, 0, 128, 0.5)",
-                    "rgba(0, 255, 255, 0.5)"
+                    "rgba(0, 0, 255, 0.5)",      // Blue
+                    "rgba(255, 0, 0, 0.5)",      // Red
+                    "rgba(0, 200, 0, 0.5)",      // Green
+                    "rgba(255, 165, 0, 0.5)",    // Orange
+                    "rgba(128, 0, 128, 0.5)",    // Purple
+                    "rgba(0, 255, 255, 0.5)",    // Cyan
+                    "rgba(255, 105, 180, 0.5)",  // Hot Pink
+                    "rgba(75, 0, 130, 0.5)",     // Indigo
+                    "rgba(255, 215, 0, 0.5)",    // Gold
+                    "rgba(139, 69, 19, 0.5)",    // Brown
+                    "rgba(0, 191, 255, 0.5)",    // Deep Sky Blue
+                    "rgba(154, 205, 50, 0.5)"    // Yellow-Green
                 ];
-                const linePalette = ["blue", "red", "green", "orange", "purple", "cyan"];
+
+                const linePalette = [
+                    "blue",
+                    "red",
+                    "green",
+                    "orange",
+                    "purple",
+                    "cyan",
+                    "hotpink",
+                    "indigo",
+                    "gold",
+                    "brown",
+                    "deepskyblue",
+                    "yellowgreen"
+                ];
+
 
                 let traces = [];
 
