ec42cbb3971d5874193085cf976d8fa764967102
Sam Molyneux <samjmolyneux@gmail.com>
2025-02-28 17:49:09 +0000

    correct roc plot call

--- eppi_text_classification/__init__.py
+++ eppi_text_classification/__init__.py
@@ -5,7 +5,7 @@ from .plots.confusion_plot import (
     binary_train_valid_confusion_plotly,
     binary_train_valid_test_confusion_plotly,
 )
-from .plots.roc import plotly_roc
+from .plots.roc import roc_plot
 from .predict import get_raw_threshold, predict_scores, raw_threshold_predict
 from .save_features_labels import get_features, get_labels, get_tfidf_and_names
 from .shap_plotter import ShapPlotter
@@ -20,7 +20,7 @@ __all__ = [
     "get_labels",
     "get_raw_threshold",
     "get_tfidf_and_names",
-    "plotly_roc",
     "predict_scores",
     "raw_threshold_predict",
+    "roc_plot",
 ]
