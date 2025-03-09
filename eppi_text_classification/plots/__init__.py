f06174e2696aca880f1462a6da3a6cf2be83afef
Sam Molyneux <samjmolyneux@gmail.com>
2025-03-09 17:53:33 +0000

    add confusion to plots init

--- eppi_text_classification/plots/__init__.py
+++ eppi_text_classification/plots/__init__.py
@@ -1,4 +1,9 @@
+# from beartype.claw import beartype_this_package
+
 from eppi_text_classification.plots.box_plot import box_plot
+from eppi_text_classification.plots.confusion_plot import (
+    binary_train_valid_confusion_plotly,
+)
 from eppi_text_classification.plots.histogram import (
     histogram_plot,
     positive_negative_scores_histogram_plot,
@@ -7,3 +12,5 @@ from eppi_text_classification.plots.learning_curve import learning_curve
 from eppi_text_classification.plots.optuna_plots import create_all_optuna_plots
 from eppi_text_classification.plots.roc import roc_plot
 from eppi_text_classification.plots.select_threshold import select_threshold_plot
+
+# beartype_this_package()
