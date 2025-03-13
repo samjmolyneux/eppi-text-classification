c824f92aa775e01b666836c0dedbda6665b7e237
Sam Molyneux <samjmolyneux@gmail.com>
2025-03-13 15:34:52 +0000

    delete prevous classification pipeline

--- pipelines/final_components/predict_unlabelled_tfidf/push_predict.py
+++
@@ -1,8 +0,0 @@
-from azure.ai.ml import load_component
-from load_azure_ml import get_azure_ml_client
-
-ml_client = get_azure_ml_client()
-
-component = load_component(source="./predict_unlabelled_tfidf.yml")
-
-ml_client.components.create_or_update(component)
