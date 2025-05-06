39989839f5c3a08ab9e46c2b33cd1bb6d99f2df5
Sam Molyneux <samjmolyneux@gmail.com>
2025-05-06 20:38:40 +0100

    new path

--- pipelines/environments/push_classifier_workbench_env_to_azure.py
+++ pipelines/environments/push_classifier_workbench_env_to_azure.py
@@ -5,14 +5,13 @@ from load_azure_ml import get_azure_ml_client
 
 ml_client = get_azure_ml_client()
 
-dependencies_dir = "./../dependencies"
 
 custom_env_name = "eppi-classifier-workbench-env"
 
 pipeline_job_env = Environment(
     name=custom_env_name,
     description="Python environment for eppi classifier workbench.",
-    conda_file=os.path.join(dependencies_dir, "eppi-classifier-workbench-env.yaml"),
+    conda_file="eppi-classifier-workbench-env.yaml",
     image="mcr.microsoft.com/azureml/openmpi5.0-ubuntu24.04:latest",
     version="1.0.2",
 )
