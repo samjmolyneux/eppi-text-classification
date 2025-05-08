491be27baaadd52465b891591575f57d65f91a44
Sam Molyneux <samjmolyneux@gmail.com>
2025-05-08 10:42:38 +0100

    refactor innit

--- eppi_text_classification/__init__.py
+++ eppi_text_classification/__init__.py
@@ -2,20 +2,20 @@
 
 # from beartype.claw import beartype_this_package
 
-from .opt import OptunaHyperparameterOptimisation, delete_optuna_study
+from .multi_process_hparam_search import MultiProcessHparamSearch, delete_optuna_study
 from .plots.confusion_plot import (
     binary_train_valid_confusion_plotly,
     binary_train_valid_test_confusion_plotly,
 )
 from .plots.roc import roc_plot
+from .plots.shap_plotter import ShapPlotter
 from .predict import get_raw_threshold, predict_scores, raw_threshold_predict
 from .save_features_labels import get_features, get_labels, get_tfidf_and_names
-from .shap_plotter import ShapPlotter
 
 # beartype_this_package()
 
 __all__ = [
-    "OptunaHyperparameterOptimisation",
+    "MultiProcessHparamSearch",
     "ShapPlotter",
     "binary_train_valid_confusion_plotly",
     "binary_train_valid_test_confusion_plotly",
