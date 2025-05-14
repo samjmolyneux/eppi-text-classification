8d64d27cb8d96d73518faecc0776ef076d29190b
Sam Molyneux <samjmolyneux@gmail.com>
2025-05-14 17:06:06 +0100

    remove pipeline_prot

--- pipelines/final_components/threshold_model_analysis/pipeline_prot.py
+++
@@ -1,8 +0,0 @@
-from load_azure_ml import get_azure_ml_client
-
-ml_client = get_azure_ml_client()
-
-prev_job = ml_client.jobs.get("ee31b20b-e5b6-4982-a490-736aba7397dd")
-print(prev_job.description)
-print(prev_job.properties)
-print(prev_job.outputs)
